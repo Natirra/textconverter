668ba2425935d52ca46c2f5a732ce7a36506259f
Natirra <start@suvorova.org>
2023-02-11 00:17:22 +0300

    textConvert

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let inputField = document.getElementById("input-field");
+let submitButton = document.getElementById("submit-button");
+let outputField = document.getElementById("output-field");
+let inputForExcludeField = document.getElementById("input-for-exclude-field");
+let excludeField = document.getElementById("exclude-field");
+let excludeButton = document.getElementById("exclude-button");
+let excludeOutputField = document.getElementById("exclude-output-field");
+
+
+submitButton.addEventListener("click", function () {
+	console.log('click')
+	let input = inputField.value;
+	let output = input.replace(/\d+\./g, '').replace(/\s/g, ', ');
+	outputField.textContent = output;
+});
+
+excludeButton.addEventListener("click", function () {
+	let input = inputForExcludeField.value.split(", ");
+	let exclude = excludeField.value.split(", ");
+	let output = input.filter(function (word) {
+		return !exclude.includes(word);
+	}).join(", ");
+
+	excludeOutputField.textContent = output;
+});
